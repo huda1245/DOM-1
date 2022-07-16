f1a62f2d39ef08572ca6afd10b0775f08cad405b
hudashahzad <hudashahzad786@gmail.com>
2022-07-16 00:57:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// prt1
+// answerno1
+// var mainContent = document.getElementById("main-content");
+// console.log(mainContent);
+// answerno2
+// console.log(mainContent.children);
+// answerno3
+// var render = document.getElementsByClassName("render")
+// render.innerHTML = "huda"
+// console.log(render.innerHTML)
+// answerno4
+// var input = document.getElementById("first-name")
+// input.value = "Huda"
+// console.log(input.value)
+// answerno5
+// var lastName = document.getElementById("last-name")
+// lastName.value = "Enter your last name"
+// console.log(lastName.value)
+// prt2
+
+// answerno1
+// var form=document.getElementById("form-content")
+// console.log(form.nodeType)  
+// answerno2
+// var lastName=document.getElementById("lastName");
+// console.log(lastName.childNodes)
+// answerno3
+// lastName.childNodes[1].innerHTML="huda";
+// console.log(lastName.childNodes[1].innerHTML)
+// answerno4
+// console.log(mainContent.firstElementChild);
+// console.log(mainContent.lastElementChild)
+// answerno5
+// console.log(lastName.nextElementSibling);
+// console.log(lastName.previousElementSibling);
+// answerno6
+// var email = document.getElementById("email")
+// console.log(email.parentNode);
